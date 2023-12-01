e497168f0bde8e35908c6de99db0033ae2807b1c
Killian K/vella <97022820+RichardGaming772@users.noreply.github.com>
2023-12-01 17:05:11 +0100

    Added idle animation

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ var speed = maxWidth / 100;
 var lastDirection = "r";
 
 var dirQueue = [];
+var nameCard = document.querySelector("#nameCard");
+var xCardEnd = roundToTwo(speed * Math.ceil(nameCard.scrollWidth / speed) + speed);
+var yCardEnd = roundToTwo(speed * Math.ceil(nameCard.scrollHeight / speed) + speed);
 
 async function init() {
     maxWidth = document.querySelector("#field").clientWidth;
@@ -26,14 +29,23 @@ async function init() {
         speed = (maxWidth / 100) * 2;
     else
         speed = (maxWidth / 100);
+    nameCard.style.width = "unset";
+    nameCard.style.height = "unset";
+    xCardEnd = roundToTwo(speed * Math.ceil(nameCard.scrollWidth / speed) + speed + speed);
+    yCardEnd = roundToTwo(speed * Math.ceil(nameCard.scrollHeight / speed) + speed + speed);
+    nameCard.style.width = xCardEnd + "px";
+    nameCard.style.height = yCardEnd + "px";
+    topPos = roundToTwo(speed * Math.ceil(nameCard.offsetTop / speed) - speed - speed);
+    leftPos = roundToTwo(speed * Math.ceil(nameCard.offsetLeft / speed) - speed - speed);
+    xStart = leftPos;
+    yStart = topPos;
+    console.log(leftPos, xStart);
     dot.style.width = speed + "px";
     dot.style.height = speed + "px";
     trails.forEach(trail => {
         trail.remove();
     });
     trails = [];
-    topPos = 0;
-    leftPos = 0;
     dot.style.top = topPos + "px";
     dot.style.left = leftPos + "px";
     direction = "r";
@@ -44,19 +56,22 @@ async function init() {
     gameEnded = false;
     lastDirection = "r";
 }
+var xStart = roundToTwo(speed * Math.ceil(nameCard.offsetLeft / speed) - speed);
+var yStart = roundToTwo(speed * Math.ceil(nameCard.offsetTop / speed) - speed);
 
 init();
 var gameId = setInterval(move, timeout);
 
 window.addEventListener('resize', function () {
-    gameEnded = true;
     clearInterval(gameId);
+    gameEnded = true;
     init().then(() => {
         gameId = setInterval(move, timeout);
     });
 });
 
 document.addEventListener("keypress", switchDir);
+
 document.addEventListener("keypress", async function (e) {
     if (gameEnded && e.code == "KeyR") {
         init().then(() => {
@@ -70,8 +85,35 @@ function random(max) {
 }
 
 var food = create("div", dot.parentNode, "", "food");
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2") + "e-2");
+}
+
 function move() {
     var test = true;
+    var downTurn = roundToTwo((speed * Math.floor(nameCard.offsetLeft / speed) + xCardEnd));
+    var leftTurn = roundToTwo((speed * Math.floor(nameCard.offsetTop / speed) + yCardEnd));
+    if (leftPos == downTurn && topPos == yStart) {
+        direction = "d";
+        sinceDirection = 0;
+        test = false;
+    }
+    if (leftPos == downTurn && topPos == leftTurn) {
+        direction = "l";
+        sinceDirection = 0;
+        test = false;
+    }
+    if (leftPos == roundToTwo(xStart - speed) && topPos == leftTurn) {
+        direction = "u";
+        sinceDirection = 0;
+        test = false;
+    }
+    if (leftPos == roundToTwo(xStart - speed) && topPos == yStart) {
+        direction = "r";
+        sinceDirection = 0;
+        test = false;
+    }
     if (dirQueue.length > 0) {
         direction = dirQueue[0];
         dirQueue.splice(0, 1);
@@ -262,49 +304,49 @@ function create(tag, parent, text, classs = null, id = null) {
 
 // Swipe Controls
 
-document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchstart', handleTouchStart, false);
 document.addEventListener('touchmove', handleTouchMove, false);
 
-var xDown = null;                                                        
+var xDown = null;
 var yDown = null;
 
 function getTouches(e) {
-  return e.touches ||
-         e.originalEvent.touches; 
-}                                                     
-                                                                         
+    return e.touches ||
+        e.originalEvent.touches;
+}
+
 function handleTouchStart(e) {
-    const firstTouch = getTouches(e)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
-                                                                         
+    const firstTouch = getTouches(e)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
 function handleTouchMove(e) {
-    if ( ! xDown || ! yDown ) {
+    if (!xDown || !yDown) {
         return;
     }
 
-    var xUp = e.touches[0].clientX;                                    
+    var xUp = e.touches[0].clientX;
     var yUp = e.touches[0].clientY;
 
     var xDiff = xDown - xUp;
     var yDiff = yDown - yUp;
-    var temp = {code: ""};
-                                                                         
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
-        if ( xDiff > 0 ) {
+    var temp = { code: "" };
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
             temp.code = "KeyA";
         } else {
             temp.code = "KeyD";
-        }                       
+        }
     } else {
-        if ( yDiff > 0 ) {
+        if (yDiff > 0) {
             temp.code = "KeyW";
-        } else { 
+        } else {
             temp.code = "KeyS";
-        }                                                                 
+        }
     }
     switchDir(temp);
     xDown = null;
-    yDown = null;                                             
+    yDown = null;
 };
